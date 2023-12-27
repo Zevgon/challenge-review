a722e75f68e32f3217b2861e0d6d630d6354888c
Joe McDonough <joemcdonoughmusic.com>
2023-12-26 22:40:49 -0500

    switch the margins on category-slabs to padding and gap assigned on the product-category-menu container.  use extend %header-font on .category-name to fix font-weight and consolidate font-weight and text-transform into one line.  Put the main-container class on the entire HeroSection component and then assign the ThinRule a width of 100vw only for mobile sized devices.  Fix padding on main-container to match design across devices, and bump up the width on the navigation row inside NavBar.  Add =small-laptop breakpoint to increase navigation row even more on those devices, to stop the links from smushing together

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -6,13 +6,13 @@ import FeaturedProductInfo from "../FeaturedProductInfo/FeaturedProductInfo";
 const HeroSection = (props) => {
   const { featuredProduct } = props;
   return (
-    <section className="hero-section col">
+    <section className="hero-section col main-container">
       <ThinRule customStyle="thin-rule-hero" />
       <FeaturedProductInfo
         featuredProduct={featuredProduct}
         newProduct
         customClasses={{
-          containerClasses: "featured-product-hero-info main-container col",
+          containerClasses: "featured-product-hero-info col",
           headerClasses: "featured-product-hero-header",
           newProductIntroClasses: "overline new-product-intro",
           teaserDescriptionClasses: "featured-product-teaser-hero",
--- src/components/ProductCategoryMenu/ProductCategoryMenu.js
+++ src/components/ProductCategoryMenu/ProductCategoryMenu.js
@@ -1,22 +1,22 @@
 import React from "react";
 import CategorySlab from "../CategorySlab/CategorySlab";
 import "./product-category-menu.sass";
-import earphonesPreviewImage from "./images/image-category-thumbnail-earphones.png";
 import headphonesPreviewImage from "./images/image-category-thumbnail-headphones.png";
 import speakerPreviewImage from "./images/image-category-thumbnail-speakers.png";
+import earphonesPreviewImage from "./images/image-category-thumbnail-earphones.png";
 
 const ProductCategoryMenu = () => {
   return (
-    <section className="main-container product-category-menu">
+    <section className="main-container product-category-menu col">
       <CategorySlab
         categoryName="headphones"
         imageName={headphonesPreviewImage}
       />
+      <CategorySlab categoryName="speakers" imageName={speakerPreviewImage} />
       <CategorySlab
         categoryName="earphones"
         imageName={earphonesPreviewImage}
       />
-      <CategorySlab categoryName="speakers" imageName={speakerPreviewImage} />
     </section>
   );
 };
