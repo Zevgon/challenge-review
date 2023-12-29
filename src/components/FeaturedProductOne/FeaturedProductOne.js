f03511d0fca8fa30ee5ac96871552a5b4b91481a
Joe McDonough <joemcdonoughmusic.com>
2023-12-28 20:48:06 -0500

    work on getting the featured-product-one section to look like the design at 1440px screen widths

--- src/components/FeaturedProductOne/FeaturedProductOne.js
+++ src/components/FeaturedProductOne/FeaturedProductOne.js
@@ -10,36 +10,35 @@ import featuredProductImageDesktop from "./images/image-speaker-zx9-desktop.png"
 const FeaturedProductOne = (props) => {
   const { featuredProduct } = props;
   return (
-    <section>
-      <div className="featured-product-one">
-        <CirclePatternSvg />
-        <div className="featured-product-one-image-and-text">
+    <section className="featured-product-one">
+      <CirclePatternSvg />
+      <div className="featured-product-one-image-and-text">
+        <div className="featured-product-one-image-container">
           <CustomImage
-            className="featured-product-one-image-mobile image-mobile"
+            className="featured-product-one-image image-mobile"
             src={featuredProductImageMobile}
             alt="featured-product-image-mobile"
           />
           <CustomImage
-            className="featured-product-one-image-tablet image-tablet"
+            className="featured-product-one-image image-tablet"
             src={featuredProductImageTablet}
             alt="featured-product-image-tablet"
           />
           <CustomImage
-            className="featured-product-one-image-desktop image-desktop"
+            className="featured-product-one-image featured-product-one-image-desktop image-desktop"
             src={featuredProductImageDesktop}
             alt="featured-product-image-desktop"
           />
-          <FeaturedProductInfo
-            featuredProduct={featuredProduct}
-            customClasses={{
-              containerClasses:
-                "featured-product-one-info col center-flex-items",
-              headerClasses: "featured-product-one-header",
-              teaserDescriptionClasses: "featured-product-one-teaser",
-              buttonData: { buttonColor: "black", buttonText: "see product" },
-            }}
-          />
         </div>
+        <FeaturedProductInfo
+          featuredProduct={featuredProduct}
+          customClasses={{
+            containerClasses: "featured-product-one-info col",
+            headerClasses: "featured-product-one-header",
+            teaserDescriptionClasses: "featured-product-one-teaser",
+            buttonData: { buttonColor: "black", buttonText: "see product" },
+          }}
+        />
       </div>
     </section>
   );
