633d79cf5ea13bbb0a88e9e1a282a03b55d0522a
Joe McDonough <joemcdonoughmusic.com>
2023-12-29 21:15:38 -0500

    set button hover styles on transparent button, make featured product three images a background-image on featured-product-three-container instead of three CustomImage components.  Decrease intrinsic height on featured-product-one for mobile and tablet by reducing padding above and below featured-product-one-teaser and decreasing margin above featured-product-info section.  Continue building styles for FeaturedProductThree component for tablet and desktop

--- src/components/FeaturedProductThree/FeaturedProductThree.js
+++ src/components/FeaturedProductThree/FeaturedProductThree.js
@@ -1,32 +1,12 @@
 import React from "react";
 import "./featured-product-three.sass";
-import CustomImage from "../CustomImage/CustomImage";
 import FeaturedProductInfo from "../FeaturedProductInfo/FeaturedProductInfo";
-import featuredProductImageMobile from "./images/image-earphones-yx1-mobile.jpg";
-import featuredProductImageTablet from "./images/image-earphones-yx1-tablet.jpg";
-import featuredProductImageDesktop from "./images/image-earphones-yx1-desktop.jpg";
 
 const FeaturedProductThree = (props) => {
   const { featuredProduct } = props;
   return (
     <section className="featured-product-three-container">
-      <div className="featured-product-three-image-container">
-        <CustomImage
-          className="featured-product-three-image featured-product-three-image-mobile image-mobile"
-          src={featuredProductImageMobile}
-          alt="featured-product-three-image-mobile"
-        />
-        <CustomImage
-          className="featured-product-three-image featured-product-three-image-tablet image-tablet"
-          src={featuredProductImageTablet}
-          alt="featured-product-three-image-tablet"
-        />
-        <CustomImage
-          className="featured-product-three-image featured-product-three-image-desktop image-desktop"
-          src={featuredProductImageDesktop}
-          alt="featured-product-three-image-desktop"
-        />
-      </div>
+      <div className="featured-product-three-image-container"></div>
       <div className="featured-product-three-text-container content-slab">
         <FeaturedProductInfo
           featuredProduct={featuredProduct}
