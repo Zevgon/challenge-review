f489c4c01456350bfbea71ec23c05b9c5128c284
Joe McDonough <joemcdonoughmusic.com>
2024-02-15 14:39:06 -0500

    remove print statements from CartContext.tsx

--- src/components/Context/CartContext.tsx
+++ src/components/Context/CartContext.tsx
@@ -36,30 +36,14 @@ export const CartProvider = ({ children }: PropsWithChildren) => {
   };
 
   const removeItemFromCart = (itemToRemove: ItemToPurchase) => {
-<<<<<<< HEAD
-    console.log("all cart items at top of function: ", itemsInCart);
     setItemsInCart(
       itemsInCart.map((itemInCart) => {
-        console.log(
-          "item in cart: ",
-          itemInCart.product,
-
-          "\n item to remove: ",
-          itemToRemove.product
-        );
-=======
-    setItemsInCart(
-      itemsInCart.map((itemInCart) => {
->>>>>>> 9592b07 (fix bug inside removeItemFromCart function by using -= instead of just - when decrementing quantity property of item object)
         return itemInCart.product.id === itemToRemove.product.id
           ? { ...itemInCart, quantity: (itemInCart.quantity -= 1) }
           : itemInCart;
       })
     );
-<<<<<<< HEAD
-    console.log("all cart items at bottom of function: ", itemsInCart);
-=======
->>>>>>> 9592b07 (fix bug inside removeItemFromCart function by using -= instead of just - when decrementing quantity property of item object)
+
     setItemsInCart(
       itemsInCart.filter((itemInCart) => {
         return itemInCart.quantity > 0;
